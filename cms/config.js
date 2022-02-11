5c4d5ea3df6f270974873aef13b70763f4ef68ba
Erwan Darves <erwan.darves@gmail.com>
2022-02-11 12:06:37 +0000

    :bug: Fix local errors

--- cms/config.js
+++ cms/config.js
@@ -2,7 +2,7 @@ export default {
   cms_manual_init: true,
   backend: {
     name: 'github',
-    repo: 'jakeprins/nextjs-netlify-cms',
+    repo: 'darvese/erwandarves.com',
     branch: 'main',
   },
   media_folder: 'public/img',
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
 import config from '../cms/config';
 const CMS = dynamic(
+  //@ts-ignore
   () =>
-    import('netlify-cms-app').then((cms) => {
+    import('netlify-cms-app').then((cms: any) => {
       cms.init({ config });
     }),
-  { ssr: false, loading: () => <p>Loading...</p> }
+  { ssr: false, loading: () => <p>Loading...</p> },
 );
 const AdminPage: React.FC = () => {
   return <CMS />;
